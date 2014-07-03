36aa5ffc8fe63eca2d5087572364c6ce2b389774
yChing <ms0777648@gmail.com>
2014-07-03 11:53:49 +0800

    initial

--- TocFinal.java
+++ TocFinal.java
@@ -15,7 +15,113 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-
 import org.json.*;
 
+public class TocFinal {
+	static ArrayList<String> keys = new ArrayList<String>();
+	static HashMap<String, Value> hashMap = new HashMap<String, Value>();
+	static JSONObject publicJSONObject;
+	static int appearTime = 0;
+	static StringBuilder publicStringBuilder = new StringBuilder();
 
+
+	static int K;
+	static int L;
+	public static void processFromPage(String httpAddress) {
+
+		URL u = null;
+		InputStream in = null;
+		InputStreamReader r = null;
+		BufferedReader br = null;
+
+		try {
+
+			u = new URL(httpAddress);
+			in = u.openStream();
+
+			r = new InputStreamReader(in, "UTF-8");
+			br = new BufferedReader(r);
+
+			String buffer = null;
+
+			while ((buffer = br.readLine()) != null) {
+				try {
+					if (keys.size() == 0)
+						keys = findOrder(buffer);
+					if (buffer.charAt(0) == '{') {
+						publicJSONObject = new JSONObject(new String(buffer));
+						combination(L, 0, 0, "");
+					}
+				} catch (Exception e) {
+				}
+			}
+
+		} catch (Exception e) {
+			e.getStackTrace();
+		} finally {
+			try {
+				u = null;
+				in.close();
+				r.close();
+				br.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+	}
+
+	public static void processFromFile(String filename) {
+		try {
+			InputStreamReader isr = new InputStreamReader(new FileInputStream(
+					filename), "UTF-8");
+			BufferedReader reader = new BufferedReader(isr);
+			try {
+				do {
+					String buffer = reader.readLine();
+					if (buffer == null)
+						break;
+
+					try {
+						if (keys.size() == 0)
+							keys = findOrder(buffer);
+						if (buffer.charAt(0) == '{') {
+							publicJSONObject = new JSONObject(
+									new String(buffer));
+							combination(L, 0, 0, "");
+						}
+					} catch (Exception e) {
+					}
+
+				} while (true);
+			} catch (Exception e) {
+			} finally {
+				reader.close();
+			}
+		} catch (Exception e) {
+		}
+	}
+
+
+
+	public static void main(String[] args) throws MalformedURLException,
+			JSONException, FileNotFoundException {
+
+		String URL = args[0];
+
+		K = Integer.valueOf(args[1]);
+
+		L = Integer.valueOf(args[2]);
+		if (isURL(URL))
+			processFromPage(URL);
+		else {
+			processFromFile(URL);
+		}
+		try {
+			outputReslut();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
